a768f23af458cdab2d66fde6757fb817cafae16a
lazarljubenovic <lazar.ljubenovic.1995@gmail.com>
2017-10-14 22:12:22 +0200

    fix: export common_form_control token

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,4 @@
 export {CommonFormsModule} from './src/common-forms.module'
 export {CommonFormDirective} from './src/common-form.directive'
 export * from './src/interfaces'
+export {COMMON_FORM_CONTROL} from './src/config'
