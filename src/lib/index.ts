c7bb16fba0f8639519da9819ede53f3527ec094e
lazarljubenovic <lazar.ljubenovic.1995@gmail.com>
2019-02-19 13:38:01 +0100

    chore: delete everything

--- src/lib/index.ts
+++
@@ -1,4 +0,0 @@
-export {CommonFormsModule} from './src/common-forms.module'
-export {CommonFormDirective} from './src/common-form.directive'
-export * from './src/interfaces'
-export {COMMON_FORM_CONTROL} from './src/config'
--- src/lib/src/common-form-control.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-// import { CommonFormControlDirective } from './common-form-control.directive';
-//
-// describe('CommonFormControlDirective', () => {
-//   it('should create an instance', () => {
-//     const directive = new CommonFormControlDirective();
-//     expect(directive).toBeTruthy();
-//   });
-// });
